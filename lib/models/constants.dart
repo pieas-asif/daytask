1ef7df8c0e2af820ec46e9eb3762f5d32af94a23
pieas-asif <pieas.asif4@gmail.com>
2021-12-17 22:08:39 +0600

    database updated

--- lib/models/constants.dart
+++ lib/models/constants.dart
@@ -4,3 +4,15 @@ class DTTheme {
   static Color black = const Color(0xFF323232);
   static Color white = const Color(0xFFE8E8E8);
 }
+
+class TaskTable {
+  static String name = "task_table";
+  static String colTaskDate = "task_date";
+  static String colIsChecked = "task_done";
+  static String colTaskTitle = "task_title";
+}
+
+class FixedTaskTable {
+  static String name = "fixed_task_table";
+  static String colTaskTitle = "task_title";
+}
